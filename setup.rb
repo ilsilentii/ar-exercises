b0d57a0624f5abfcf30ff947e638a38f49bdd9c7
ilsilentii <travis.vdm@outlook.com>
2017-10-10 15:39:34 -0600

    completed excerise 1-4

--- setup.rb
+++ setup.rb
@@ -9,9 +9,9 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
-  database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  database: 'ruby',
+  username: 'travis',
+  password: 'password',
   host: 'localhost',
   port: 5432,
   pool: 5,
