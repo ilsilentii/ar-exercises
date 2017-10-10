f20eb05a709336dd8b313f17396524e9037033ad
ilsilentii <travis.vdm@outlook.com>
2017-10-10 16:03:45 -0600

    completed excerise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 15)
+@store1.employees.create(first_name: "Travis", last_name: "Vandermeer", hourly_rate: 80)
+@store1.employees.create(first_name: "Mike", last_name: "Hunt", hourly_rate: 20)
 
+@store2.employees.create(first_name: "Richard", last_name: "Face", hourly_rate: 30)
+@store2.employees.create(first_name: "Alyssa", last_name: "Thomas", hourly_rate: 40)
+@store2.employees.create(first_name: "Bobo", last_name: "Irwin", hourly_rate: 25)
